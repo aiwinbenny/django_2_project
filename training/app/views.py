585bee2add6dab37d7592984dbef4efd80ede2a0
aiwinbenny <aiwinbenny33@gmail.com>
2020-12-07 17:57:00 +0530

    added form model

--- training/app/views.py
+++ training/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from app.models import User
+from app.forms import NewUser
 
 # Create your views here.
 
@@ -7,7 +8,19 @@ def fun(request):
     dict = {'text':'go to /page'}
     return  render(request,'app/page1.html',context = dict)
 
+# def fun2(request):
+#     table = User.objects.all()
+#     dict = {'table': table}
+#     return render(request,'app/page2.html',context= dict)
+
+
 def fun2(request):
-    table = User.objects.all()
-    dict = {'table': table}
-    return render(request,'app/page2.html',context= dict)
+    form = NewUser()
+    if request.method == 'POST':
+        form = NewUser(request.POST)
+        if form.is_valid():
+            form.save(commit = True)
+            return fun(request)
+        else:
+            print("error from invalid")
+    return render(request,'app/page2.html',context = {'form':form})
